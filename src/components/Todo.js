8dec06ba27ba90937d88a82531cf6424b289438e
huyennguyenthanh <ngthanhhuyen1707@gmail.com>
2022-06-05 17:36:24 +0700

    Change Todo status

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -27,19 +27,26 @@ function Todo() {
     /* テストコード 終了 */
   ]);
 
+  const handleOnCheck = checked => {
+    const newItems = items.map(item => {
+      if (item.key === checked.key){
+        item.done = !item.done;
+      }
+      return item;
+    })
+    putItems(newItems);
+  }
+
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
       {items.map(item => (
-        // <label className="panel-block">
-        //     <input type="checkbox" />
-        //     {item.text}
-        // </label>
         <TodoItem
           key={item.key}
           item={item}
+          onCheck={handleOnCheck}
         />
       ))}
       <div className="panel-block">
